93ec8366bb5edab636676f2a72be724e0a0915db
Aishwarya <amuthyala511@gmail.com>
2020-10-26 18:34:27 +0530

    calculated winning and losing amount of the gambler for 20 days

--- Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -9,22 +9,42 @@ public class GamblingSimulation
 	static float stake_value = 0.5f;
 	public static final int WIN = 1;
 	
-	public void bet()
+	public void betDays()
 	{
-		Stake = Stake-Bet;
-		Random random = new Random();
-		int betCheck = random.nextInt(2);
-		if(betCheck == WIN)
+		int day = 0, winningAmount = 0, losingAmount = 0;
+		while(day < 20)
 		{
-			Stake = (int) (Stake + (Stake * stake_value));
-			System.out.println("Gambler won the bet");
-			System.out.println("Money left with Gambler: "+Stake);
-		}
-		else
-		{
-			Stake = (int) (Stake - (Stake * stake_value));
-			System.out.println("Gambler lost the bet");
-			System.out.println("Money left with Gambler: "+Stake);
+			day++;
+			Stake = Stake-Bet;
+			Random random = new Random();
+			int betCheck = random.nextInt(2);
+			if(Stake > 0)
+			{
+				int betAmount = (int) (Stake * stake_value);
+				if(betCheck == WIN)
+				{
+					Stake = Stake + betAmount + ( 2 * Bet );
+					System.out.println("Gambler won the bet");
+					System.out.println("Money left with Gambler: "+Stake);
+					winningAmount = winningAmount + betAmount + (2 * Bet);
+				}
+				else
+				{
+					Stake = Stake - betAmount;
+					System.out.println("Gambler lost the bet");
+					System.out.println("Money left with Gambler: "+Stake);
+					losingAmount = losingAmount + betAmount + Bet;
+				}
+				System.out.println("Amount for betting on Day "+day+" is: "+betAmount);
+				System.out.println("Total amount won by Gambler: "+winningAmount);
+				System.out.println("Total amount lost by Gambler: "+losingAmount);
+			}
+			else
+			{
+				System.out.println("Cannot play..., no money left to bet");
+				System.out.println("Total amount won by Gambler: "+winningAmount);
+				System.out.println("Total amount lost by Gambler: "+losingAmount);
+			}
 		}
 	}
 	
@@ -33,7 +53,7 @@ public class GamblingSimulation
 		System.out.println("Welcome to Gambling Simulation Project");
 		System.out.println("Gambler started with "+Stake+" rupees");
 		GamblingSimulation gamble = new GamblingSimulation();
-		gamble.bet();
+		gamble.betDays();
 	}
 	
 }
