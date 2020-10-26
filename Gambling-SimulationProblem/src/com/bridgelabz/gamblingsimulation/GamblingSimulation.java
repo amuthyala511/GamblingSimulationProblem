ea58fd9d32119f7da448fc7d52d2feeea561e952
Aishwarya <amuthyala511@gmail.com>
2020-10-26 23:30:31 +0530

    calculated maximum win amount on luckiest day and maximum loss amount on unluckiest day

--- Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.gamblingsimulation;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class GamblingSimulation
@@ -8,20 +9,24 @@ public class GamblingSimulation
 	static int Bet = 1;
 	static float stake_value = 0.5f;
 	public static final int WIN = 1;
+	static int win[] = new int[30];
+	static int loss[] = new int[30];
 	static int winCount = 0, loseCount = 0;
 	
 	public void betDays()
 	{
-		int day = 0, winningAmount = 0, losingAmount = 0;
-		while(day < 30)
+		int day = 0, i = 0; 
+		int winningAmount = 0, losingAmount = 0;
+		while(day < 30 && i < 30)
 		{
+			int winamount, lossamount;
 			day++;
 			Stake = Stake-Bet;
 			Random random = new Random();
 			int betCheck = random.nextInt(2);
+			int betAmount = (int) (Stake * stake_value);
 			if(Stake > 0)
 			{
-				int betAmount = (int) (Stake * stake_value);
 				if(betCheck == WIN)
 				{
 					Stake = Stake + betAmount + ( 2 * Bet );
@@ -29,6 +34,10 @@ public class GamblingSimulation
 					System.out.println("Money left with Gambler: "+Stake);
 					winningAmount = winningAmount + betAmount + (2 * Bet);
 					winCount++;
+					winamount = ( betAmount + ( 2 * Bet ) );
+					win[i] = winamount;
+					System.out.println("amount won on Day "+day+" is: "+win[i]);
+					i++;
 				}
 				else
 				{
@@ -37,6 +46,10 @@ public class GamblingSimulation
 					System.out.println("Money left with Gambler: "+Stake);
 					losingAmount = losingAmount + betAmount + Bet;
 					loseCount++;
+					lossamount = betAmount + Bet;
+					loss[i] = lossamount;
+					System.out.println("amount lost on Day "+day+" is: "+loss[i]);
+					i++;
 				}
 				System.out.println("Amount for betting on Day "+day+" is: "+betAmount);
 				System.out.println("Total amount won by Gambler: "+winningAmount);
@@ -44,6 +57,7 @@ public class GamblingSimulation
 				
 				System.out.println("Total number of wins by Gambler: "+winCount);
 				System.out.println("Total number of loses by Gambler: "+loseCount);
+				
 			}
 			else
 			{
@@ -54,6 +68,32 @@ public class GamblingSimulation
 				System.out.println("Total number of loses by Gambler: "+loseCount);
 			}
 		}
+		System.out.println("winning amounts: "+Arrays.toString(win));
+		System.out.println("losing amounts: "+Arrays.toString(loss));
+	}
+		
+	public void maxAmount()
+	{
+		int winmax = 0, lossmax = 0;
+		int windayCount = 0, lossdayCount = 0;
+		for(int counter=0;counter<win.length;counter++)
+		{
+			if(win[counter]>winmax)
+			{
+				winmax = win[counter];
+				windayCount = counter + 1;
+			}
+		}
+		System.out.println("maxmimun amount "+winmax+" is won on Day "+windayCount);
+		for(int counter=0;counter<loss.length;counter++)
+		{
+			if(loss[counter]>lossmax)
+			{
+				lossmax = loss[counter];
+				lossdayCount = counter + 1;
+			}
+		}
+		System.out.println("maxmimun amount "+lossmax+" is lost on Day "+lossdayCount);
 	}
 	
 	public static void main(String[] args)
@@ -62,6 +102,7 @@ public class GamblingSimulation
 		System.out.println("Gambler started with "+Stake+" rupees");
 		GamblingSimulation gamble = new GamblingSimulation();
 		gamble.betDays();
+		gamble.maxAmount();
 	}
 	
 }
