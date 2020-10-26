a7b5dbc80843f38bf4418617bb991a1f89b57c5a
Aishwarya <amuthyala511@gmail.com>
2020-10-26 17:16:57 +0530

    Started the Gambling Simulation Project

--- Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -2,8 +2,11 @@ package com.bridgelabz.gamblingsimulation;
 
 public class GamblingSimulation
 {
+	public static final int Stake = 100;
+	public static final int Bet = 1;
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Gambling Simulation Project");
+		System.out.println("Gambler started with "+Stake+" rupees");
 	}
 }
