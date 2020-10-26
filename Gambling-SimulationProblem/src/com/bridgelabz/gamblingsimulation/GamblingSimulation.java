c719390bff26ef3fe83364ed08d955165623d3e4
Aishwarya <amuthyala511@gmail.com>
2020-10-26 17:36:13 +0530

    checked if the gambler won or lost the bet

--- Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -1,12 +1,37 @@
 package com.bridgelabz.gamblingsimulation;
 
+import java.util.Random;
+
 public class GamblingSimulation
 {
-	public static final int Stake = 100;
-	public static final int Bet = 1;
+	static int Stake = 100;
+	static int Bet = 1;
+	public static final int WIN = 1;
+	
+	public void bet()
+	{
+		Stake = Stake-Bet;
+		Random random = new Random();
+		int betCheck = random.nextInt(2);
+		if(betCheck == WIN)
+		{
+			Stake = Stake+2;
+			System.out.println("Gambler won");
+			System.out.println("Money left with Gambler: "+Stake);
+		}
+		else
+		{
+			System.out.println("Gambler lost");
+			System.out.println("Money left with Gambler: "+Stake);
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Gambling Simulation Project");
 		System.out.println("Gambler started with "+Stake+" rupees");
+		GamblingSimulation gamble = new GamblingSimulation();
+		gamble.bet();
 	}
+	
 }
