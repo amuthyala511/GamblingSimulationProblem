30277c88a7624ab2c50a9e950af1205564c4ce61
Aishwarya <amuthyala511@gmail.com>
2020-10-26 18:51:26 +0530

    calculated number of wins, loses and amount of the gambler for a month

--- Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ Gambling-SimulationProblem/src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -8,11 +8,12 @@ public class GamblingSimulation
 	static int Bet = 1;
 	static float stake_value = 0.5f;
 	public static final int WIN = 1;
+	static int winCount = 0, loseCount = 0;
 	
 	public void betDays()
 	{
 		int day = 0, winningAmount = 0, losingAmount = 0;
-		while(day < 20)
+		while(day < 30)
 		{
 			day++;
 			Stake = Stake-Bet;
@@ -24,26 +25,33 @@ public class GamblingSimulation
 				if(betCheck == WIN)
 				{
 					Stake = Stake + betAmount + ( 2 * Bet );
-					System.out.println("Gambler won the bet");
+					System.out.println("\nGambler won the bet");
 					System.out.println("Money left with Gambler: "+Stake);
 					winningAmount = winningAmount + betAmount + (2 * Bet);
+					winCount++;
 				}
 				else
 				{
 					Stake = Stake - betAmount;
-					System.out.println("Gambler lost the bet");
+					System.out.println("\nGambler lost the bet");
 					System.out.println("Money left with Gambler: "+Stake);
 					losingAmount = losingAmount + betAmount + Bet;
+					loseCount++;
 				}
 				System.out.println("Amount for betting on Day "+day+" is: "+betAmount);
 				System.out.println("Total amount won by Gambler: "+winningAmount);
 				System.out.println("Total amount lost by Gambler: "+losingAmount);
+				
+				System.out.println("Total number of wins by Gambler: "+winCount);
+				System.out.println("Total number of loses by Gambler: "+loseCount);
 			}
 			else
 			{
-				System.out.println("Cannot play..., no money left to bet");
+				System.out.println("\nCannot play..., no money left to bet on Day "+day);
 				System.out.println("Total amount won by Gambler: "+winningAmount);
 				System.out.println("Total amount lost by Gambler: "+losingAmount);
+				System.out.println("Total number of wins by Gambler: "+winCount);
+				System.out.println("Total number of loses by Gambler: "+loseCount);
 			}
 		}
 	}
